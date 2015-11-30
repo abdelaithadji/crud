f8d27bf01b06e350d652e0db35179bfc52df9afc
abdellahaithadji <abdellahaithadji@192.168.1.119>
2015-11-29 19:04:04 -0500

    add animate.css library

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
     <!-- Bootstrap -->
     <link href="css/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <link href="css/animate/animate.min.css" rel="stylesheet">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0-beta.2/angular.min.js"></script>
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -16,11 +17,8 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <style type='text/css'>
-        .btn-primary {
-            margin:0 auto;
-        }
-    </style>
+    
+    
   </head>
   <body ng-app="myApp" ng-controller="myCtrl">
       <div class="jumbotron">
@@ -84,7 +82,7 @@
                  </div>
               </td>
               <td></td>
-              <td><button type="button" class="btn btn-success" ng-click="addRow()">+</button></td>
+              <td><button type="button" class="btn btn-success center-block" ng-click="addRow()"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></button></td>
             </tr>
             <tr>
           </tbody>
@@ -98,10 +96,11 @@
             </tr>
           </thead>
           <tbody>
-            <tr ng-repeat="rowContent in rows">
+            <tr class="animated infinite bounceIn" ng-repeat="rowContent in rows">
               <td>{{rowContent}}</td>
               <td>Doe</td>
               <td>john@example.com</td>
+              <td><button type="button" class="btn btn-danger center-block" ng-click="removeRow()"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button></td>
             </tr>
            
           </tbody>
@@ -130,6 +129,11 @@
                     //console.log("test");
                     $scope.rows.push('Row ' + $scope.counter);
                     $scope.counter++;
+                  }
+                  $scope.removeRow = function(){
+                   
+                    console.log('remove');
+                   
                   }
                 }, function errorCallback(response) {
                   $scope.firstName = "Nop";
